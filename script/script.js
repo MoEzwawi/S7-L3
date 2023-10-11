613a0b1f39e36eccadca092c5769571ff033fa47
MoEzwawi <mezwawi96@gmail.com>
2023-10-11 18:20:24 +0200

    adds cards

--- script/script.js
+++ script/script.js
@@ -1,10 +1,26 @@
-const title = document.getElementsByClassName('card-title')[0]
-const text = document.getElementsByClassName('card-text')[0]
-
+const myRow = document.getElementsByClassName('row')[0]
 
 fetch('https://striveschool-api.herokuapp.com/books')
 .then(res=>res.json())
 .then((bookData)=>{
-    title.innerText = bookData[0].title
-    text.innerText = bookData[0].category + '  ' + bookData[0].price
+    console.log(bookData)
+    bookData.forEach(book => {
+        const newCol = document.createElement('div')
+        newCol.classList.add('col','col-12','col-sm-6','col-md-4','col-lg-3','col-xl-2')
+        // const imgUrl = book.img
+        newCol.innerHTML=`<div class="card" style="height: 500px">
+        <img src="${book.img}" class="card-img-top" height=60%>
+        <div class="card-body">
+          <h5 class="card-title">${book.price}</h5>
+          <p class="card-text">${book.title}</p>
+          <a href="#" class="btn btn-primary">Discard</a>
+        </div>
+      </div>`
+        myRow.appendChild(newCol)
+    });
 })
+
+
+const deleteMe = () => {
+    
+}
